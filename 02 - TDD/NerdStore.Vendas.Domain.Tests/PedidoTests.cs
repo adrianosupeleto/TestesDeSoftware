a8a850810b308f52e6b51c27530e2fd488f866f1
Adriano Supeleto <adriano.supeleto.17@gmail.com>
2023-03-31 08:56:06 -0300

    Incrementando unidades do Pedido e status Rascunho

--- 02 - TDD/NerdStore.Vendas.Domain.Tests/PedidoTests.cs
+++ 02 - TDD/NerdStore.Vendas.Domain.Tests/PedidoTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Xunit;
 
@@ -12,14 +13,36 @@ namespace NerdStore.Vendas.Domain.Tests
         public void AdicionarItemPedido_NovoPedido_DeveAtualizarValor()
         {
             // Arrange
-            var pedido = new Pedido();
-            var pedidoItem = new PedidoItem(Guid.NewGuid(),"Produto Teste",2,100);
+            var pedido = Pedido.PedidoFactory.NovoPedidoRascunho(Guid.NewGuid());
+            var pedidoItem = new PedidoItem(Guid.NewGuid(), "Produto Teste", 2, 100);
 
             // Act
             pedido.AdicionarItem(pedidoItem);
 
             // Assert
-            Assert.Equal(200,pedido.ValorTotal);
+            Assert.Equal(200, pedido.ValorTotal);
+        }
+
+        [Fact(DisplayName = "Adicionar Item Pedido Existente")]
+        [Trait("Categoria", "Pedido Tests")]
+        public void AdicionarItemPedido_ItemExistenteo_DeveIncrementarUnidadesSomarValor()
+        {
+            // Arrange
+            var pedido = Pedido.PedidoFactory.NovoPedidoRascunho(Guid.NewGuid());
+            var produtoId = Guid.NewGuid();
+            var pedidoItem = new PedidoItem(produtoId, "Produto Teste", 2, 100);
+            var pedidoItem2 = new PedidoItem(produtoId, "Produto Teste", 1, 100);
+
+
+            pedido.AdicionarItem(pedidoItem);
+
+            // Act
+            pedido.AdicionarItem(pedidoItem2);
+
+            // Assert
+            Assert.Equal(300, pedido.ValorTotal);
+            Assert.Equal(1,pedido.PedidoItems.Count);
+            Assert.Equal(3,pedido.PedidoItems.FirstOrDefault(p => p.ProdutoId == produtoId).Quantidade);
         }
     }
 }
